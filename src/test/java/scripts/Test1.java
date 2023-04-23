e0eb7e71b9c4e1de640723245fbc051cef9cb776
surajkumar <surajkumar@localhost>
2023-04-24 02:09:58 +0530

    Advance selenium

---
+++ src/test/java/scripts/Test1.java
@@ -0,0 +1,13 @@
+package scripts;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test1 {
+		@Test
+		public void tc1() {
+			Reporter.log("Hello Selenium",true);
+		}
+	}
+
